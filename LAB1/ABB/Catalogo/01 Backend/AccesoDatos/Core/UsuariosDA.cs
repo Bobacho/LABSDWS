51e2fae3420488b00c541365719dc673da0fd5d3
AULA F.DE ING.DE SISTEMAS E INF <AULAFISI@UNMSM.pe>
2024-04-17 20:12:12 -0500

    Avanzando para hasta la pag24

--- LAB1/ABB/Catalogo/01 Backend/AccesoDatos/Core/UsuariosDA.cs
+++ LAB1/ABB/Catalogo/01 Backend/AccesoDatos/Core/UsuariosDA.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Entidades.Core;
+using Utiles.Helpers;
 using Microsoft.IdentityModel.Protocols;
 using System.Security.Cryptography.X509Certificates;
 using System.Net;
@@ -75,6 +76,35 @@ namespace AccesoDatos.Core
                 Console.WriteLine(ex);
             }
             return ListaEntidad;
+        }
+        public int GetUsuarioId(string pUsuario,string pPassword)
+        {
+            try{
+                byte [] UserPass = EncriptacionHelper.EncriptarByte(pPassword);
+                int returnedVal = 0;
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder()
+                {
+                    DataSource = "34.172.53.168",
+                    TrustServerCertificate = true,
+                    UserID = "sqlserver",
+                    Password = "(n?V(R1f<;#XqlCh",
+                    InitialCatalog = "DBCatalogo"
+                };
+                SqlConnection connection = new SqlConnection(builder.ConnectionString);
+                SqlCommand command = new SqlCommand("paUsuario_BuscarCodUserClave",connection);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@ParamUsuario",pUsuario);
+                command.Parameters.AddWithValue("@ParamPass",UserPass);
+                connection.Open();
+                returnedVal = Convert.ToInt32(command.ExecuteScalar());
+                connection.Close();
+                return Convert.ToInt32(returnedVal);
+            }catch (Exception ex)
+            {
+                string innerException = ex.InnerException == null ? "" : ex.InnerException.ToString();
+                Console.WriteLine(innerException);
+                return -1;
+            }
         }
     }
 }
--- LAB1/ABB/Catalogo/01 Backend/LogicaNegocio/Core/UsuariosLN.cs
+++ LAB1/ABB/Catalogo/01 Backend/LogicaNegocio/Core/UsuariosLN.cs
@@ -1,31 +1,32 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Entidades.Core;
+using ABB.Catalogo.Entidades.Core;
 using AccesoDatos.Core;
 
-namespace LogicaNegocio.Core
-{
-    public class UsuariosLN
-    {
+namespace ABB.Catalogo.LogicaNegocio.Core{
+    public class UsuariosLN{
         public List<Usuarios> ListarUsuarios()
         {
-            List <Usuarios> lista = new List<Usuarios> ();
-            try
-            {
-                UsuariosDA usuarios = new UsuariosDA();
+            List<Usuarios> lista = new List<Usuarios> ();
+            try{
+                UsuariosDA usuarios = new UsuariosDa();
                 return usuarios.ListarUsuarios();
+            }
+            catch(Exception ex)
+            {
+                string innerException = (ex.InnerException == null) 
+                ? "" : ex.InnerException.ToString();
+                return lista;
+            }
+        }
+        public int GetUsuarioId(string pUsuario,string pPassword)
+        {
+            try{
+                UsuariosDA usuario = new UsuariosDA();
+                return usuario.GetUsuarioId(pUsuario,pPassword);
             }
             catch (Exception ex)
             {
-                string innerException = (ex.InnerException == null) ? "" :
-                ex.InnerException.ToString();
-                //Logger.paginaNombre = this.GetType().Name;
-                //Logger.Escribir(&quot;Error en Logica de Negocio: &quot; + ex.Message + &quot;. &quot;
-                return lista;
+                string innerException = (ex.InnerException == null) ? "" : ex.InnerException.ToString(); 
+                return -1;
             }
         }
     }
